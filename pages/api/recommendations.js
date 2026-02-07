3bd94b5ba7b879fd79b775bc87ff3744e7638742
tanmay <tanmay.s@unlocktech.co.in>
2026-02-07 18:56:07 +0530

    images updated

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -3,34 +3,34 @@ const recommendationCard = [
   {
     id: 0,
     name: 'DigiKhata SDK',
-    image: "images/shahmir.jfif",
-    designation: 'React JS | NEXT JS | Node | GraphQL',
+    image: "images/paypoint.png",
+    designation: 'React JS | Rest APIs | iFrame',
     view: "Developed a secure wallet onboarding SDK enabling users to create digital wallets through Aadhaar OTP or biometric-based KYC, integrated via an iFrame model.Implemented API-driven workflows and AES encryption to ensure data security and smooth integration.",
     // linkednURL: "https://www.linkedin.com/in/shahmir-minhas/"
   },
   {
     id: 1,
     name: 'VR Maritime',
-    image: "images/sikandar.jpeg",
-    designation: 'Data Scientist | Machine Learning Engineer | Python Developer',
-    view: "I had the privilege of working alongside Osama on multiple university projects, and I must say, his talent and work ethic truly stood out. Recently, we were both part of the dynamic team at JMM Technologies, where Osama's professionalism and attention to detail shone brightly. I wholeheartedly endorse Osama for any opportunity that calls for a highly skilled and dedicated individual.",
-    linkednURL: "https://www.linkedin.com/in/sikandar-hayat-381407179/"
+    image: "images/vrm.png",
+    designation: 'React.js | Redux | AsyncThunk | ReactRouter',
+    view: "Built a web application for inventory, finance, and vessel management with efficient state handling using Redux.Implemented modules for invoicing, currency exchange, and vendor-customer management, streamlining shipping operations. Used React Router for seamless navigation and React Hook Form for optimized form handling and validation.",
+    // linkednURL: "https://www.linkedin.com/in/sikandar-hayat-381407179/"
   },
   {
     id: 2,
     name: 'Hitachi TCO tool',
-    image: "images/nasir.jpg",
+    image: "images/hitachi.png",
     designation: 'MERN | JavaScript | Tailwind | Sass | Bootstrap',
     view: "Built a Total Cost of Ownership analysis tool to compare transformer designs based on cost, energy losses, GHG emissions, IRR, and payback period.Enabled evaluation of multiple transformer units under various decarbonization scenarios, supporting data-driven engineering decisions.",
-    linkednURL: "https://www.linkedin.com/in/nasirkhan22/"
+    // linkednURL: "https://www.linkedin.com/in/nasirkhan22/"
   },
   {
     id: 3,
     name: 'Loankhata / Monarch Finance Loans',
-    image: "images/muhammad.jpeg",
-    designation: 'MERN Stack Developer at Productbox',
-    view: "I highly recommend Osama Javed for web frontend development positions. Their expertise in ReactJS and Next.js, combined with their professionalism and dedication, make them an invaluable asset to any team.",
-    linkednURL: "https://www.linkedin.com/in/muhammadullahafridi/"
+    image: "images/loankhata.ico",
+    designation: 'Role-Based Permission Access-Control | React.js | Authentication & Authorization',
+    view: "Developed end-to-end loan application workflows including OTP authentication, PAN/Aadhaar verification, bank validation, KFS generation, and e-signature. with a focus on performance optimization and reliability.Designed optimized multi-step user journeys to improve conversion rates, compliance, and user experience.",
+    // linkednURL: "https://www.linkedin.com/in/muhammadullahafridi/"
   },
 ]
 export default function handler(req, res) {
